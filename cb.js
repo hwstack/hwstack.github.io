939e6f4c4399efcb3ee7d1a25cef76512875d70d
mytasks <platypusnetwork@gmail.com>
2014-01-31 15:57:59 -0500

    rxhn
    fxdcghvbj

--- cb.js
+++ cb.js
@@ -1,16 +1,3 @@
 function doThing(id){
-	
-	var isChecked = document.getElementById(id).checked;
-	
-	switch(isChecked){
-		
-	case(true):
-		document.cookie="isChecked" + id + "=1; expires=Thu, 3 Jul 2014 12:00:00 GMT"
-		break;
-	case(false):
-		document.cookie"isChecked" + id + "=0; expires =Thu, 3 Jul 2014 12:00:00 GMT"
-		break;
-	}
-	
-	
+	document.cookie = "isChecked" + id + "=" + (document.getElementById(id).checked ? 1 : 0) + "; expires=Thu, 3 Jul 2014 12:00:00 GMT";
 }
