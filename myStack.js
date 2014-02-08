cd661e5caf5ddff57e4bdc19e8466fe05ae092be
jczimm <jczimm.alias@gmail.com>
2014-02-08 18:37:20 -0500

    Reused doc for gen

--- myStack.js
+++ myStack.js
@@ -1,28 +1,10 @@
-function add() {
-	var key = $("#key").val();
-	var task = $("#task").val();
-	if(!key){
-		alert("Error W00PS: Class must be specified!");
-		$("#key").focus();
-		return;
-	}
-	$.jStorage.set(key, task);
-	reDraw();
-}
-
-function flush_data(){
-	$.jStorage.flush();
-	reDraw();
-}
-
-function reDraw(){
-	var task = $("#task").val();
-	var del = $("<a href='javascript:flush_data()'>[x]</a>");
-
-	$( "<tr><td>" + task + "</td><td>" + del + "</td></tr>" ).insertAfter( "#tulemused" );
-}
-
-
-//var toInsert = "<div class="entry"><span>" + dueDate + "</span>&nbsp;<span>" + classTag + "</span><br /><span>" + taskInfo + "</span></div>";
-//$(".entry").before(toInsert);
-//var win = window.open('about:blank','','height=400,width=400,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');
+function output(){
+        		var date=document.getElementById('date').value;
+        		var key=document.getElementById('class').value;
+        		var task=document.getElementById('task').value;
+        		document.getElementById('output').value="
+        		
+        			<div class='key'>
+        				<span class='date'>"+date+"</span>&nbsp;<span class='class'>"+key+"</span><br />
+        				<span class='task'>"+task+"</span></div>";
+        	}
